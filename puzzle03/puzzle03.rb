94e9c7964de3a1cb1da4af01eb958675ecb53bf5
Florian Oberleitner <florian.oberleitner@gmail.com>
2015-12-08 23:07:23 +0100

    Combined solutions to puzzle03; some refactoring

--- puzzle03/puzzle03.rb
+++ puzzle03/puzzle03.rb
@@ -1,12 +1,11 @@
 #!/usr/bin/env ruby -w
 
 class Navigator
-  def initialize
-    @x = 0
-    @y = 0
-    # visits are store with a string key of format 'X_Y'
-    # corresponding to the coordinates on the 2D-map
-    @visits = { '0_0' => 1 }
+  def initialize(x = 0, y = 0)
+    @x = x
+    @y = y
+    @visits = {}
+    add_visit
   end
 
   attr_accessor :visits
@@ -41,6 +40,8 @@ class Navigator
   end
 
   def add_visit(x = @x, y = @y)
+    # visits are store with a string key of format 'X_Y'
+    # corresponding to the coordinates on the 2D-map
     key = "#{x}_#{y}"
     visits = @visits[key] || 0
     @visits[key] = visits + 1
@@ -51,12 +52,19 @@ class Navigator
   end
 end
 
-nav = Navigator.new
+nav_overall = Navigator.new
+nav_santa = Navigator.new
+nav_robo = Navigator.new
 instructions = nil
 File.open('puzzle03_input.txt') do |input|
   instructions = input.read.split(//)
 end
 
-instructions.each { |dir| nav.move(dir) }
+instructions.each_index do |index|
+  instruction = instructions[index]
+  nav_overall.move(instruction)
+  index.even? ? nav_santa.move(instruction) : nav_robo.move(instruction)
+end
 
-puts 'Houses with at least one present: ' + nav.houses_with_presents.to_s
+puts "Puzzle03 Pt01: # of houses with presents: #{nav_overall.visits.size}"
+puts "Puzzle03 Pt02: # of houses with presents: #{nav_santa.visits.merge(nav_robo.visits).size}"
