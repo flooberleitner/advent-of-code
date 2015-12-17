2df132ac3448e778ecc93e5902285b09d46532dc
Florian Oberleitner <florian.oberleitner@gmail.com>
2015-12-17 07:16:58 +0100

    Refactor

--- puzzle17/puzzle17.rb
+++ puzzle17/puzzle17.rb
@@ -1,12 +1,18 @@
 #!/usr/bin/env ruby
 
-containers = [50, 44, 11, 49, 42, 46, 18, 32, 26, 40, 21, 7, 18, 43, 10, 47, 36, 24, 22, 40]
+containers = [
+  50, 44, 11, 49, 42, 46, 18, 32, 26, 40,
+  21, 7, 18, 43, 10, 47, 36, 24, 22, 40
+]
 
-possibilities = (2..(containers.size - 1)).each_with_object([]) do |combo_size, memo|
-  containers.combination(combo_size).each { |combo| memo << combo if combo.reduce(:+) == 150 }
+possibilities = (2..(containers.size - 1)).reduce([]) do |memo, combo_size|
+  memo.concat(containers.combination(combo_size).select do |combo|
+    combo.reduce(:+) == 150
+  end)
 end
 
 puts "Puzzle17 Part1: #{possibilities.size} possibilities"
 
 min_size = possibilities.map(&:size).min
-puts "Puzzle17 Part2: #{possibilities.count { |p| p.size == min_size }} possibilities of size #{min_size}"
+cnt = possibilities.count { |p| p.size == min_size }
+puts "Puzzle17 Part2: #{cnt} possibilities of size #{min_size}"
