c146cb165156031c4e4171b6138b4bfa637d7209
Florian Oberleitner <florian.oberleitner@gmail.com>
2015-12-17 23:07:43 +0100

    Refactor - limit combination scope

--- puzzle17/puzzle17.rb
+++ puzzle17/puzzle17.rb
@@ -3,9 +3,18 @@
 containers = [
   50, 44, 11, 49, 42, 46, 18, 32, 26, 40,
   21, 7, 18, 43, 10, 47, 36, 24, 22, 40
-]
+].sort
 
-possibilities = (2..(containers.size - 1)).reduce([]) do |memo, combo_size|
+# Limit search scope to max amount of containers needed
+# This speeds up the search through all container combinations
+combination_limit = 0
+containers.each_with_object([]) do |container, memo|
+  combination_limit += 1
+  memo << container
+  break if memo.reduce(:+) >= 150
+end
+
+possibilities = (2..combination_limit).reduce([]) do |memo, combo_size|
   memo.concat(containers.combination(combo_size).select do |combo|
     combo.reduce(:+) == 150
   end)
