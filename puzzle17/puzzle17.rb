45fb795d5aef12fad47c14e6beb6e7c1d75b61cf
Florian Oberleitner <florian.oberleitner@gmail.com>
2015-12-17 06:32:50 +0100

    Refactor

--- puzzle17/puzzle17.rb
+++ puzzle17/puzzle17.rb
@@ -2,11 +2,10 @@
 
 containers = [50, 44, 11, 49, 42, 46, 18, 32, 26, 40, 21, 7, 18, 43, 10, 47, 36, 24, 22, 40]
 
-combinations = (2..(containers.size / 2).ceil).each_with_object([]) do |cnt, memo|
-  containers.combination(cnt).each { |combo| memo << combo }
+possibilities = (2..(containers.size / 2).ceil).each_with_object([]) do |cnt, memo|
+  containers.combination(cnt).each { |combo| memo << combo if combo.reduce(:+) == 150 }
 end
 
-possibilities = combinations.reject { |p| p.reduce(:+) != 150 }
 puts "Puzzle17 Part1: #{possibilities.size} possibilities"
 
 min_size = possibilities.map(&:size).min
