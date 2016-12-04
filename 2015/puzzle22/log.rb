bc78ba754a8196df7f17fcb3e5d3d7d178015495
Florian Oberleitner <florian.oberleitner@gmail.com>
2016-12-04 03:15:13 +0100

    - Default Log Level is WARN - Log to File instead of STDOUT - Added global .gitignore containing excluding everything .log

--- 2015/puzzle22/log.rb
+++ 2015/puzzle22/log.rb
@@ -1,13 +1,15 @@
 require 'logger'
 
 module MyLogger
+  include Logger::Severity
+
   class << self
     attr_writer :logger
 
     def log
-      @logger ||= Logger.new($stdout).tap do |log|
+      @logger ||= Logger.new('puzzle02.log').tap do |log|
         log.progname = 'AOC2015/22'
-        log.level = Logger::DEBUG
+        log.level = Logger::WARN
       end
     end
   end
