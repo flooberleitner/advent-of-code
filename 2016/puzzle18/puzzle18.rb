cf391f59443c2c4b0be4787af979a1ff10b82add
Florian Oberleitner <florian.oberleitner@gmail.com>
2016-12-18 11:03:20 +0100

    Refactor 2016/18 Less RAM usage, still pretty slow for Part2

--- 2016/puzzle18/puzzle18.rb
+++ 2016/puzzle18/puzzle18.rb
@@ -1,20 +1,30 @@
 #!/usr/bin/env ruby
 
-INPUT = ''.freeze
-
 class TileSolver
   def initialize(input, line_count)
     @input = input
     @line_count = line_count
     @rows = [@input]
-    create_rows
+    @save_tile_count = nil
   end
 
-  def create_rows
-    (1...@line_count).each { |idx| @rows[idx] = create_row(@rows[idx - 1]) }
+  def save_tiles_count
+    return @save_tile_count if @save_tile_count
+    @save_tile_count = 0
+
+    line = @input
+    (0...@line_count).each do
+      @save_tile_count += count_save_tiles(line)
+      line = create_row(line)
+    end
+    @save_tile_count
   end
 
-  def create_row(input)
+  private def count_save_tiles(line)
+    line.delete('^').size
+  end
+
+  private def create_row(input)
     tmp = ".#{input}."
     out = ''
     (1...(tmp.size - 1)).each do |idx|
@@ -29,17 +39,15 @@ class TileSolver
     end
     out
   end
-
-  def save_tiles_count
-    # puts '=' * (@input.size + 4)
-    # @rows.each_with_index { |r, i| puts "#{i.to_s.rjust(2)}: #{r}" }
-    # puts '=' * (@input.size + 4)
-    @rows.map { |r| r.delete('^').size }.inject(0, &:+)
-  end
 end
 
 INPUT = '.^^^.^.^^^.^.......^^.^^^^.^^^^..^^^^^.^.^^^..^^.^.^^..^.^..^^...^.^^.^^^...^^.^.^^^..^^^^.....^....'.freeze
-puts "Test 1: #{TileSolver.new('..^^.', 3).save_tiles_count} (corr: 6)"
-puts "Test 2: #{TileSolver.new('.^^.^.^^^^', 10).save_tiles_count} (corr: 38)"
-puts "Part 1: #{TileSolver.new(INPUT, 40).save_tiles_count} (corr: 2013)"
-puts "Part 2: #{TileSolver.new(INPUT, 400_000).save_tiles_count} (corr: 20006289)"
+[
+  ['Test 1', '..^^.', 3, 6],
+  ['Test 2', '.^^.^.^^^^', 10, 38],
+  ['Part 1', INPUT, 40, 2013],
+  ['Part 2', INPUT, 400_000, 20_006_289],
+].each do |p|
+  res = TileSolver.new(p[1], p[2]).save_tiles_count
+  puts "#{p[0]}: #{res}#{res == p[3] ? '' : "(corr: #{p[3]})"}"
+end
