14c10ab1c94e4b0e00f1196c7f7fcf59a783485f
Florian Oberleitner <florian.oberleitner@gmail.com>
2017-12-03 22:26:49 +0100

    Update AOC17 template

--- 2017/puzzle-template/puzzleXX.rb
+++ 2017/puzzle-template/puzzleXX.rb
@@ -1,14 +1,45 @@
 #!/usr/bin/env ruby
 
-open(ARGV[0]) do |input|
-  # Get and sanitize input
-  data = input.readlines.map(&:strip)
+# Declare the number of the AOC17 puzzle
+PUZZLE = 0
 
-  # Process data
-  res1 = 0
-  res2 = 0
+# Declare all runs to be done for this puzzle
+RUNS = {
+  test: {
+    skip: true,
+    input: 'input_test.txt',
+    target: 0
+  },
+  puzzle01: {
+    skip: true,
+    input: 'input.txt',
+    target: 0
+  },
+  puzzle02: {
+    skip: true,
+    input: 'input.txt',
+    target: 0
+  }
+}.freeze
 
-  # Print result
-  puts "AOC17-XX/1: #{res1} (Corr: ???)"
-  puts "AOC17-XX/2: #{res2} (Corr: ???)"
+# Make each run
+RUNS.each do |name, data|
+  # skip run?
+  if data[:skip]
+    puts "Skipped '#{name}'"
+    next
+  end
+
+  # open input data and process it
+  open(data[:input]) do |input|
+    # Read all input lines and sanitize
+    data = input.readlines.map(&:strip)
+
+    # Process data
+    res = 0
+
+    # Print result
+    success_msg = res == data[:target] ? 'succeeded' : 'failed'
+    puts "AOC17-#{PUZZLE}/#{name} #{success_msg}: #{res} (Target: #{data[:target]})"
+  end
 end
