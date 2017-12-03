d51366bb8756d2e8ae8599349b61f418ce38025e
Florian Oberleitner <florian.oberleitner@gmail.com>
2017-12-03 15:09:38 +0100

    Updated AOC17 template

--- 2017/puzzle-template/puzzleXX.rb
+++ 2017/puzzle-template/puzzleXX.rb
@@ -5,6 +5,10 @@ open(ARGV[0]) do |input|
   data = input.readlines.map(&:strip)
 
   # Process data
+  res1 = 0
+  res2 = 0
 
   # Print result
+  puts "AOC17-XX/1: #{res1} (Corr: ???)"
+  puts "AOC17-XX/2: #{res2} (Corr: ???)"
 end
