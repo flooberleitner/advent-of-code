9adcca4b207e23173f04c1fe51d089eb2ba4d5a3
Florian Oberleitner <florian.oberleitner@gmail.com>
2017-12-15 00:59:29 +0100

    Refactor AOC17 10 (knot hash moved to own lib file) in preparation for AOC17 13

--- 2017/puzzle10/puzzle10_2.rb
+++ 2017/puzzle10/puzzle10_2.rb
@@ -1,40 +1,10 @@
 #!/usr/bin/env ruby
 
-class Array
-  def knot_hash(sizes:, rounds: 1)
-    dup = clone
-    rotate_cnt = 0 # used to reverse the overall rotation after we are finished
-    skip_size = 0
-    rounds.times do
-      sizes.each do |size|
-        dup.replace(dup.shift(size).reverse + dup)
-        steps = size + skip_size
-        dup.rotate!(steps)
-        rotate_cnt += steps
-        skip_size += 1
-      end
-    end
-    # last we rotate the hash back to start position
-    dup.rotate!(-rotate_cnt)
-  end
-
-  def sparse_hash(sizes:)
-    knot_hash(
-      sizes: sizes,
-      rounds: 64
-    )
-  end
-
-  def dense_hash(sizes:)
-    sparse_hash(sizes: sizes)
-      .each_slice(16).map { |s| format('%02x', s.reduce(:^)) }
-      .join
-  end
-end
+require_relative '../../lib/knot_hash'
 
 # Declare the number of the AOC17 puzzle
 PUZZLE = 10
-POSTFIX_SIZES = [17, 31, 73, 47, 23].freeze
+POSTFIX_KEY = [17, 31, 73, 47, 23].map(&:chr).join.freeze
 
 # Declare all runs to be done for this puzzle
 {
@@ -59,7 +29,7 @@ POSTFIX_SIZES = [17, 31, 73, 47, 23].freeze
     target: '63960835bcdc130f0b66d7ff4f6a5a8e'
   },
   puzzle02: {
-    sizes: '31, 2, 85, 1, 80, 109, 35, 63, 98, 255, 0, 13, 105, 254, 128, 33',
+    sizes: '31,2,85,1,80,109,35,63,98,255,0,13,105,254,128,33',
     hash_size: 256,
     target: '28e7c4360520718a5dc811d3942cf1fd'
   }
@@ -70,10 +40,7 @@ POSTFIX_SIZES = [17, 31, 73, 47, 23].freeze
     next
   end
 
-  sizes = run_pars[:sizes].delete(' ').each_byte.to_a + POSTFIX_SIZES
-  input = (0...run_pars[:hash_size]).to_a
-
-  res = input.dense_hash(sizes: sizes)
+  res = (run_pars[:sizes] + POSTFIX_KEY).knot_hash
 
   # Print result
   success_msg = res == run_pars[:target] ? 'succeeded' : 'failed'
---
+++ lib/knot_hash.rb
@@ -0,0 +1,25 @@
+class String
+  def knot_hash(width: 256, rounds: 64)
+    input = (0...width).to_a
+
+    rotation_total = 0 # used to reverse the overall rotation after we are finished
+    skip_size = 0
+    rounds.times do
+      each_byte.to_a.each do |slice_size|
+        input.replace(input.shift(slice_size).reverse + input)
+        rotation_steps = slice_size + skip_size
+        input.rotate!(rotation_steps)
+        rotation_total += rotation_steps
+        skip_size += 1
+      end
+    end
+    # Last we rotate the hash back to start position
+    # slice it up in chunks, XOR the chunks and join
+    # the result to a hex string.
+    input
+      .rotate!(-rotation_total)
+      .each_slice(16)
+      .map { |s| format('%02x', s.reduce(:^)) }
+      .join
+  end
+end
