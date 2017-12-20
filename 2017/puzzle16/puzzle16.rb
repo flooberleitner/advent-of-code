7ab3c5fe4837baf78f0ef36be7480da421c4d217
Florian Oberleitner <florian.oberleitner@gmail.com>
2017-12-20 01:36:22 +0100

    AOC17 16 Part 2

--- 2017/puzzle16/puzzle16.rb
+++ 2017/puzzle16/puzzle16.rb
@@ -5,27 +5,56 @@ require 'english'
 class Processor
   def initialize(to_char: 'p')
     @to_char = to_char
-    reset
-  end
-
-  def reset
+    @instructions = []
     @programms = ('a'..@to_char).to_a
+    @history = [memory]
   end
 
-  def process_instructions(instructions)
+  def compile(instructions)
     instructions.each do |instruction|
       case instruction
       when /^s(?<amount>\d+)$/
-        m = $LAST_MATCH_INFO
-        spin(m[:amount].to_i)
+        @instructions << {
+          cmd: :spin,
+          args: [
+            $LAST_MATCH_INFO[:amount].to_i
+          ]
+        }
       when %r{^x(?<pos1>\d+)/(?<pos2>\d+)$}
-        m = $LAST_MATCH_INFO
-        exchange(m[:pos1].to_i, m[:pos2].to_i)
+        @instructions << {
+          cmd: :exchange,
+          args: [
+            $LAST_MATCH_INFO[:pos1].to_i,
+            $LAST_MATCH_INFO[:pos2].to_i
+          ]
+        }
       when %r{^p(?<prog1>\w+)/(?<prog2>\w+)$}
-        m = $LAST_MATCH_INFO
-        partner(m[:prog1], m[:prog2])
+        @instructions << {
+          cmd: :partner,
+          args: [
+            $LAST_MATCH_INFO[:prog1],
+            $LAST_MATCH_INFO[:prog2]
+          ]
+        }
       else
         raise "Instruction '#{instruction}' can not be matched."
+      end
+    end
+  end
+
+  def next(runs: 1)
+    runs.times do |run|
+      @instructions.each do |instruction|
+        send(instruction[:cmd], *instruction[:args])
+      end
+
+      if @history.include? memory
+        offset = @history.index(memory)
+        cycle_size = @history.size - offset
+        remaining_runs = runs - run
+        return @history[(remaining_runs % cycle_size) + offset - 1]
+      else
+        @history << memory
       end
     end
     memory
@@ -65,14 +94,14 @@ PUZZLE = 16
     input: 's1,x3/4,pe/b',
     to_char: 'e',
     target1: 'baedc',
-    target2: 'FooBar'
+    target2: 'abcde'
   },
   puzzle: {
     skip: false,
     input: 'input.txt',
     to_char: 'p',
     target1: 'kpbodeajhlicngmf',
-    target2: 'FooBar'
+    target2: 'ahgpjdkcbfmneloi'
   }
 }.each do |run_name, run_pars|
   # skip run?
@@ -92,17 +121,17 @@ PUZZLE = 16
             [run_pars[:input]]
           end.map { |l| l.split(/,/) }.flatten
 
-  # Process data
   processor = Processor.new(to_char: run_pars[:to_char])
-  res1 = processor.process_instructions(input)
+  processor.compile(input)
+
+  res1 = processor.next
+  success_msg1 = res1 == run_pars[:target1] ? 'succeeded' : 'failed'
+  puts "AOC17-#{PUZZLE}/#{run_name}/1 #{success_msg1}: #{res1} (Target: #{run_pars[:target1]})"
+
   # TODO for part 2 we have to check when the sequence starts to loop and from
   # there on calculate what would be the state at the 1 billionth repetition
-  res2 = processor.memory
-
-  # Print result
-  success_msg1 = res1 == run_pars[:target1] ? 'succeeded' : 'failed'
+  res2 = processor.next(runs: 999_999_999)
   success_msg2 = res2 == run_pars[:target2] ? 'succeeded' : 'failed'
-  puts "AOC17-#{PUZZLE}/#{run_name}/1 #{success_msg1}: #{res1} (Target: #{run_pars[:target1]})"
   puts "AOC17-#{PUZZLE}/#{run_name}/2 #{success_msg2}: #{res2} (Target: #{run_pars[:target2]})"
   puts '=' * 50
 end
